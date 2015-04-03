3f5aad84734746f139d1e29ba1da39775554fb0a
Pawan Dubey <pawanthegunner@gmail.com>
2015-04-03 15:07:53 +0530

    Connection established

--- src/main/java/com/pawandubey/dredd/Dredd.java
+++ src/main/java/com/pawandubey/dredd/Dredd.java
@@ -15,17 +15,43 @@
  */
 package com.pawandubey.dredd;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Pawan Dubey pawandubey@outlook.com
  */
 public class Dredd {
 
+    private static final String DB_PASSWORD = "root";
+    private static final String DB_USERNAME = "root";
+    private static final String DBMS = "mysql";
+    private static final String DB_SERVER = "localhost";
+    private static final String DB_PORT = "3306";
+    private static final String DB_NAME = "dredd";
     /**
      * @param args the command line arguments
+     * @throws java.lang.ClassNotFoundException
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws ClassNotFoundException {
+        //Class.forName("com.mysql.jdbc.Driver");
+        Connection dbConnection = null;
+        Properties connectionProperties = new Properties();
+        connectionProperties.put("user", DB_USERNAME);
+        connectionProperties.put("password", DB_PASSWORD);
+        String url = "jdbc:" + DBMS + "://" + DB_SERVER + ":" + DB_PORT + "/" + DB_NAME;
+        try {
+            dbConnection = DriverManager.getConnection(url, connectionProperties);
+            System.out.println(dbConnection.getClientInfo());
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(Dredd.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
